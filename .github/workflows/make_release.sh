17bab9246e190ca019c30f9d40615d1a09d0b50b
GitHub Action <action@github.com>
2019-11-21 15:03:19 -0500

    f

--- .github/workflows/make_release.sh
+++ .github/workflows/make_release.sh
@@ -6,6 +6,8 @@ git config --local user.name "GitHub Action"
 is_major_release=$(git log -1 --pretty=%B | grep "major release")
 is_minor_release=$(git log -1 --pretty=%B | grep "minor release")
 
+echo $is_major_release
+echo $is_minor_release
 # grep text of most recent commit message
 if [ $is_major_release ]
 then
