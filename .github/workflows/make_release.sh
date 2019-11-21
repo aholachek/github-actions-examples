7da752c98a0e35306eb1af830914a14e13260714
GitHub Action <action@github.com>
2019-11-21 15:07:53 -0500

    f

--- .github/workflows/make_release.sh
+++ .github/workflows/make_release.sh
@@ -9,10 +9,10 @@ is_minor_release=$(git log -1 --pretty=%B | grep "minor release")
 echo $is_major_release
 echo $is_minor_release
 # grep text of most recent commit message
-if [ $is_major_release ]
+if [ "$is_major_release" = "major release" ]
 then
   npm version major
-elif [ $is_minor_release ]
+elif [ "$is_minor_release" = "minor release" ]
 then
   npm version minor
 else
