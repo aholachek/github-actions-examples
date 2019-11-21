2d105a9463e3ca1d06be81276c444f9495636a58
GitHub Action <action@github.com>
2019-11-21 15:29:56 -0500

    ignore case

--- .github/workflows/make_release.sh
+++ .github/workflows/make_release.sh
@@ -3,8 +3,8 @@
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 
-is_major_release=$(git log -1 --pretty=%B | grep "major release")
-is_minor_release=$(git log -1 --pretty=%B | grep "minor release")
+is_major_release=$(git log -1 --pretty=%B | grep -i "major release")
+is_minor_release=$(git log -1 --pretty=%B | grep -i "minor release")
 
 # grep text of most recent commit message
 if [ "$is_major_release" = "major release" ]
