7bf5dba3db9918ed0f671f6ce8572919107fcbab
GitHub Action <action@github.com>
2019-11-21 15:00:59 -0500

    ok

---
+++ .github/workflows/make_release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+
+is_major_release=$(git log -1 --pretty=%B | grep "major release")
+is_minor_release=$(git log -1 --pretty=%B | grep "minor release")
+
+# grep text of most recent commit message
+if [ $is_major_release ]
+then
+  npm version major
+elif [ $is_minor_release ]
+then
+  npm version minor
+else
+  npm version patch
+fi
